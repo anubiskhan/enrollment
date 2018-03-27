0d3598a0eb5bc00d3f4c700fd9cf99b434ae2e84
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-03-27 14:59:10 -0600

    Adds function to edit student with tests

--- spec/features/user_edits_student_spec.rb
+++ spec/features/user_edits_student_spec.rb
@@ -4,8 +4,12 @@ describe 'user edits a student' do
   it 'user sees student name has changed' do
     student = Student.create!(name: 'Gargamel')
 
-    visit student_path(student)
+    visit edit_student_path(student)
 
-    expect(page).to have_content(student.name)
+    fill_in 'student[name]', with: 'Lemagrag'
+    click_on 'submit'
+
+    expect(current_path).to eq(student_path(student))
+    expect(page).to have_content('Lemagrag')
   end
 end
